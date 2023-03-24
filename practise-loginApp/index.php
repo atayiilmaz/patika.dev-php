097061000efdb23ff92cbbf3fe88d9ea5cc55291
Ata Berk Yılmaz <126782062+atayiilmaz@users.noreply.github.com>
2023-03-25 00:21:47 +0300

    regular update

--- practise-loginApp/index.php
+++ practise-loginApp/index.php
@@ -1,3 +1,13 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['login']) || $_SESSION['login'] == false) {
+    header("Location: login.php");
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
--- practise-loginApp/islem.php
+++ practise-loginApp/islem.php
@@ -1,2 +1,26 @@
 <?php
+session_start();
+include 'helper.php';
 
+$user = [
+    'atayilmaz' => '123456'
+];
+
+if(get('islem') == 'giris') {
+
+    $_SESSION['username'] = post('username');
+    $_SESSION['password'] = post('password');
+
+    if(!post('username')){
+        $_SESSION['error'] = 'Lütfen kullanıcı adı giriniz';
+        header('Location: login.php');
+        exit();
+    } else if(!post('password')){
+        $_SESSION['error'] = 'Lütfen şifrenizi giriniz';
+        header('Location: login.php');
+        exit();
+    }
+
+}
+
+?>
